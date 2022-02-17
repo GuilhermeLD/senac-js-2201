2fd2ba3ed4b948eee3dbbd37bfaadc1e9031dd56
Guilherme Lima David <guilherme.david@spread.com.br>
2022-02-16 22:07:16 -0300

    aula de hoje JS bala

---
+++ app.js
@@ -0,0 +1,45 @@
+//comentário de linha
+
+/*
+comentário em bloco 
+
+*/
+
+const xpto = 10;
+
+console.log(xpto);
+
+let minhaVar = "Olá";
+
+minhaVar = minhaVar + " Mundo!";
+
+console.log(minhaVar);
+
+if (true) {
+  console.log("IF Verdadeiro");
+} else {
+  console.log("IF falso");
+}
+
+function diretoNoElemento() {
+  console.log("olá mundo");
+}
+
+for (let i = 0 ; i < 10 ; i++) {
+    console.log('repetição ' + i)
+}
+
+
+
+function condLooping() {
+    if (true) {
+        console.log("IF Verdadeiro");
+      } else {
+        console.log("IF falso");
+      }
+      for (let i = 0 ; i < 10 ; i++) {
+        console.log('repetição ' + i)
+      }
+    }
+
+    condLooping();
